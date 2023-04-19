1b2a6ad5ba2bd16633ecceda0a3b9489732e7456
Tanmay49 <79889809+Tanmay49@users.noreply.github.com>
2023-04-19 20:30:57 +0530

    Appointment Screen (#5)

---
+++ pages/appointment.js
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+function AppointmentBooking() {
+  const [date, setDate] = useState(new Date());
+  const [startTime, setStartTime] = useState(new Date().toLocaleTimeString());
+  let finalStartTime="";
+  let finalEndTime="";
+  const [endTime, setEndTime] = useState(new Date().toLocaleTimeString());
+
+  const handleDateChange = (date) => {
+    setDate(date);
+  };
+
+  const handleStartTimeChange = (event) => {
+    setStartTime(event.target.value);
+  };
+
+  const handleEndTimeChange = (event) => {
+    setEndTime(event.target.value);
+   
+  };
+
+  const handleSubmit = (event) => {
+    console.log(date.toISOString());
+    finalStartTime =date.toISOString().substring(0, 11)+startTime+".000"+"Z";
+    finalEndTime =date.toISOString().substring(0, 11)+endTime+".000"+"Z";
+    console.log(finalStartTime);
+    console.log(finalEndTime);
+    event.preventDefault();
+    // handle form submission here
+  };
+
+  return (
+    <div>
+      <h1>Appointment Booking</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="date">Date:</label>
+        <Calendar
+          onChange={handleDateChange}
+          value={date}
+        />
+
+        <label htmlFor="startTime">Start Time:</label>
+        <input
+          type="time"
+          id="startTime"
+          name="startTime"
+          value={startTime}
+          onChange={handleStartTimeChange}
+        />
+
+        <label htmlFor="endTime">End Time:</label>
+        <input
+          type="time"
+          id="endTime"
+          name="endTime"
+          value={endTime}
+          onChange={handleEndTimeChange}
+        />
+
+        <button type="submit">Book Appointment</button>
+      </form>
+    </div>
+  );
+}
+
+export default AppointmentBooking;
